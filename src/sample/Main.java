69d429bcfb5729e574d8dc078ce91451cff95f76
demo <nshemoul@gmail.com>
2016-01-07 13:49:46 -0800

    all classes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Login");
-        Scene scene = new Scene(root,1600, 1000);
-       scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Cimena City");
+        Scene scene = new Scene(root,1000, 1000);
+        scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
 
---
+++ src/sample/maincontroller.java
@@ -0,0 +1,39 @@
+package sample;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+import java.awt.event.MouseEvent;
+
+/*
+public class maincontroller {
+
+    @FXML
+    Button logoBtn;
+
+    @FXML
+    HBox hbLogin;
+
+
+    @FXML
+    void initialize(){
+        logoBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event)
+        hbLogin.setVisible(false);
+    }
+
+});
+
+        Platform.runLater(new Runnable(){
+        @Override
+    public void run() {
+        lblLogin.requestFocus();
+        }
+
+        });
+
+        } }
+*/
