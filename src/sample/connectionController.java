212db85b41b5205e1b29fe7a03b4becd08430b3e
yossisilb <yossisilb@gmail.com>
2016-01-12 19:05:50 +0200

    Initial Commit

---
+++ src/sample/connectionController.java
@@ -0,0 +1,53 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class connectionController {
+
+    @FXML
+    Button btnConnect;
+    @FXML
+    VBox vbConnect;
+    @FXML
+    Label lblLogin;
+    @FXML
+    TextField tfIP;
+    @FXML
+    TextField tfSocket;
+
+    @FXML
+    public void handleCloseButtonAction() {
+        Stage stage = (Stage) btnConnect.getScene().getWindow();
+        System.out.println(tfIP.getText());
+        System.out.println(tfSocket.getText());
+        tfIP.setText(tfIP.getText());
+        stage.close();
+    }
+
+    @FXML
+    void initialize(){
+        btnConnect.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                handleCloseButtonAction();
+
+            }
+        });
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                lblLogin.requestFocus();
+            }
+        });
+    }
+
+}
